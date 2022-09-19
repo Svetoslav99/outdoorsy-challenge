7d6b0a58ef7b02423367ce56db379ee927cc6265
Svetoslav99 <52759406+Svetoslav99@users.noreply.github.com>
2022-09-19 13:20:43 +0300

    Created 'Props' type and destructured the list and error.
    Added if check for error and if we have an error - returns an h3 element with the error message.
    
    Created the list mapping and creation of the 'Card' component to which is passed a unique key, image prop and title prop.

--- src/components/features/List.tsx
+++ src/components/features/List.tsx
@@ -1,12 +1,22 @@
 import classes from './List.module.css';
+import { Card } from '../index';
+import { List as ListProps } from '../../utils/types';
 
-const List: React.FC = (props) => {
+type Props = {
+    list: ListProps[];
+    error: string;
+    className: string;
+};
+
+const List: React.FC<Props> = ({ list, error }) => {
+    if (error) {
+        return <h3>{error}</h3>;
+    }
 
     return (
         <>
-            {props.list.map((item) => (
-        // TODO: create your cards here 
-
+            {list.map((item, index) => (
+                <Card key={`${item.title}_${index}`} image={item.image} title={item.title} />
             ))}
         </>
     );
