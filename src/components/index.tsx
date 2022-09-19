9710412b005c85a1d8e74dbe69648a3476100476
Svetoslav99 <52759406+Svetoslav99@users.noreply.github.com>
2022-09-19 13:12:30 +0300

    Added 'Card' component export.

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,3 +1,4 @@
 export { default as Layout } from './layout/Layout';
 export { default as Search } from './features/Search';
 export { default as List } from './features/List';
+export { default as Card } from './ui/Card';
