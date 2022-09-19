a5416e3d345ad71798a820251a5549000992ae31
Svetoslav99 <52759406+Svetoslav99@users.noreply.github.com>
2022-09-19 13:11:47 +0300

    Added 'useState' hook for both error handling and list population. The 'Search' component will populate the list with data and the error(if there is one) and the 'List' component will render the corresponding jsx.

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import classes from './Layout.module.css';
 import { Search, List } from '../index';
+import { List as ListProps } from '../../utils/types';
 
 const Layout: React.FC = () => {
+    const [list, setList] = useState<ListProps[]>([]);
+    const [error, setError] = useState('');
 
     return (
         <>
             <section className={classes['search-container']}>
-                <Search />
+                <Search setList={setList} setError={setError} />
             </section>
             <section className={classes['list-items-container']}>
-                <List />
+                <List  list={list} error={error}/>
             </section>
         </>
     );
