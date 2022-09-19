9b0d5df959b34e17b2314adafd7c0eaa09929f9a
Svetoslav99 <52759406+Svetoslav99@users.noreply.github.com>
2022-09-19 12:12:46 +0300

    Added useEffect hook that is fetching based on the user input.
    Added datalist with options that I found manually. For future improvement - can be created a script that is finding all different filter types and this datalist is populated with dynamic options.

--- src/components/features/Search.tsx
+++ src/components/features/Search.tsx
@@ -1,9 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import classes from './Search.module.css';
 
 const Search: React.FC = () => {
     const [inputValue, setInputValue] = useState('');
+
+    useEffect(() => {
+        const timer = setTimeout(async () => {
+            try {
+                const res = await fetch(`https://search.outdoorsy.com/rentals?filter[keywords]=${inputValue}`);
+
+                if (!res.ok) {
+                    throw new Error('Something went wrong!');
+                }
+
+                const data = await res.json();
+
+                console.log('data: ', data);
+            } catch (error) {
+                const message = error instanceof Error ? error.message : String(error);
+                console.log(message);
+            }
+        }, 500);
+
+        return () => clearTimeout(timer);
+    }, [inputValue]);
 
     const inputChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
         setInputValue(e.target.value);
@@ -12,7 +33,20 @@ const Search: React.FC = () => {
     return (
         <div className={classes.container}>
             <label className={classes.label}>Search: </label>
-            <input className={classes.input} value={inputValue} type='text' onChange={inputChangeHandler} />
+            <input
+                className={classes.input}
+                value={inputValue}
+                type='text'
+                onChange={inputChangeHandler}
+                list='filter-types'
+                placeholder='trailer'
+            />
+
+            <datalist id='filter-types'>
+                <option value='trailer' />
+                <option value='fifth-wheel' />
+                <option value='camper-van' />
+            </datalist>
         </div>
     );
 };
