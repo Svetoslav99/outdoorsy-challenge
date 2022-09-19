d68fd2e8b806eed0c783195e1b3e5a9e8241d329
Svetoslav99 <52759406+Svetoslav99@users.noreply.github.com>
2022-09-19 14:45:48 +0300

    Added one more useState for managing the loading state. When a user searches and waits for response with the list, then a h3 element is appearing for a brief moment.

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -7,14 +7,15 @@ import { List as ListProps } from '../../utils/types';
 const Layout: React.FC = () => {
     const [list, setList] = useState<ListProps[]>([]);
     const [error, setError] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
 
     return (
         <>
             <section className={classes['search-container']}>
-                <Search setList={setList} setError={setError} />
+                <Search setList={setList} setError={setError} setIsLoading={setIsLoading} />
             </section>
             <section className={classes['list-items-container']}>
-                <List  list={list} error={error}/>
+                <List list={list} error={error} isLoading={isLoading} />
             </section>
         </>
     );
