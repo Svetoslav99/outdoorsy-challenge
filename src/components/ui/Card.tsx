72c2f778a3d23d85c73279782a6de35250276c92
Svetoslav99 <52759406+Svetoslav99@users.noreply.github.com>
2022-10-02 13:45:42 +0300

    Added 'vehicleType', 'pricePerDay' and 'presentmentCurr' to the article output.

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -6,12 +6,19 @@ type Props = {
     image: string;
     title: string;
     className: string;
+    vehicleType: string;
+    pricePerDay: number;
+    presentmentCurr: string;
 };
 
-const Card: React.FC<Props> = ({ image, title, className }) => (
+const Card: React.FC<Props> = ({ image, title, className, vehicleType, pricePerDay, presentmentCurr }) => (
     <article className={className}>
         <img className={classes.image} src={image} alt={`image for ${title}`} />
         <h3 className={classes.title}>{title}</h3>
+        <p>{vehicleType}</p>
+        <p>
+            {pricePerDay} | {presentmentCurr}
+        </p>
     </article>
 );
 
