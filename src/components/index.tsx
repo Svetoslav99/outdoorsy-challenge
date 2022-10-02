63f9f857f010806c0f39633766bb0b57dcab53a9
Svetoslav99 <52759406+Svetoslav99@users.noreply.github.com>
2022-10-02 13:49:25 +0300

    Added default exports for the './features/ListItemsOrder' and for './features/PriceFilter'

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,4 +1,6 @@
 export { default as Layout } from './layout/Layout';
 export { default as Search } from './features/Search';
 export { default as List } from './features/List';
+export { default as ListItemsOrder } from './features/ListItemsOrder';
+export { default as PriceFilter } from './features/PriceFilter';
 export { default as Card } from './ui/Card';
