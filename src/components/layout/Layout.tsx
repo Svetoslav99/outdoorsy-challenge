191fbcb02b60609f013f8a5d27af80bf2d48b12a
Svetoslav99 <52759406+Svetoslav99@users.noreply.github.com>
2022-10-02 13:22:09 +0300

    Added itemsCount useState hook.
    Changed css class 'search-container' to 'filter-container'.
    
    Added to 'Search' component the 'setItemsCount' react dispatcher.
    
    Added 'itemsCount' to the 'List' component.

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -8,14 +8,20 @@ const Layout: React.FC = () => {
     const [list, setList] = useState<ListProps[]>([]);
     const [error, setError] = useState('');
     const [isLoading, setIsLoading] = useState(false);
+    const [itemsCount, setItemsCount] = useState(0);
 
     return (
         <>
-            <section className={classes['search-container']}>
-                <Search setList={setList} setError={setError} setIsLoading={setIsLoading} />
+            <section className={classes['filter-container']}>
+                <Search
+                    setList={setList}
+                    setError={setError}
+                    setIsLoading={setIsLoading}
+                    setItemsCount={setItemsCount}
+                />
             </section>
             <section className={classes['list-items-container']}>
-                <List list={list} error={error} isLoading={isLoading} />
+                <List list={list} error={error} isLoading={isLoading} itemsCount={itemsCount} />
             </section>
         </>
     );
