7c89fa34471ceab9a1328264acba5aa4f9fe51e2
Svetoslav99 <52759406+Svetoslav99@users.noreply.github.com>
2022-10-02 13:41:20 +0300

    Added useState hooks regarding sorting the items in asc or desc order, min and max price filtering.
    Search -> useEffect: Added a timeout constant, which value depends on if the user tries to sort or is typing in an input field.
    
    Search -> useEffect: Added pull out from the api of 'vehicleType', 'pricePerDay' and 'presentmentCurr'.
    
    Search -> useEffect: Added checks for wheter the user is using the input fields for min and max price for filtering.
    
    Search -> useEffect: Added checks for sorting the 'formattedData' if the user clicked on asc. order or on desc. order filter button.
    
    Added the rendering of a new component 'PriceFilter', which takes care of the min and max price input fields.
    
    Added the rendering of a new component 'ListItemsOrder', which takes care of the items ordering (asc. or desc. order).

--- src/components/features/Search.tsx
+++ src/components/features/Search.tsx
@@ -2,45 +2,83 @@ import React, { useState, useEffect } from 'react';
 
 import classes from './Search.module.css';
 import { List } from '../../utils/types';
+import { PriceFilter, ListItemsOrder } from '../index';
 
 type Props = {
     setList: React.Dispatch<React.SetStateAction<List[]>>;
     setError: React.Dispatch<React.SetStateAction<string>>;
     setIsLoading: React.Dispatch<React.SetStateAction<boolean>>;
+    setItemsCount: React.Dispatch<React.SetStateAction<number>>;
 };
 
-const Search: React.FC<Props> = ({ setList, setError, setIsLoading }) => {
-    const [inputValue, setInputValue] = useState('');
+const Search: React.FC<Props> = ({ setList, setError, setIsLoading, setItemsCount }) => {
+    const [searchInputValue, setSearchInputValue] = useState('');
+    const [minPriceInputValue, setMinPriceInputValue] = useState(0);
+    const [maxPriceInputValue, setMaxPriceInputValue] = useState(0);
+    const [sortItemsAsc, setSortItemsAsc] = useState(false);
+    const [sortItemsDesc, setSortItemsDesc] = useState(false);
 
     useEffect(() => {
+        const timeout = sortItemsAsc || sortItemsDesc ? 300 : 500;
+
         const timer = setTimeout(async () => {
-            if (!inputValue) {
+            if (!searchInputValue) {
                 setList([]);
                 return;
             }
 
             try {
                 setIsLoading(true);
-                const res = await fetch(`https://search.outdoorsy.com/rentals?filter[keywords]=${inputValue}`);
+                const res = await fetch(`https://search.outdoorsy.com/rentals?filter[keywords]=${searchInputValue}`);
 
                 if (!res.ok) {
                     throw new Error('Something went wrong!');
                 }
 
-                const { data, included } = await res.json();
+                const parsedData = await res.json();
 
                 const formattedData: List[] = [];
 
-                data.forEach((item: any) => {
+                parsedData.data.forEach((item: any) => {
                     const rentalName: string = item.attributes.name;
-                    const rentalImgRelation = included.find(
+                    const rentalImgRelation = parsedData.included.find(
                         (rentalImg: any) => item.relationships.primary_image.data.id === rentalImg.id
                     );
                     const rentalImgUrl: string = rentalImgRelation?.attributes.url;
+                    const vehicleType: string = item.attributes.display_vehicle_type;
+                    const pricePerDay: number = item.attributes.price_per_day; // price in cents
+                    const pricePerDayFormatted = pricePerDay / 100;
+                    const presentmentCurr: string = item.attributes.presentment_currency;
 
-                    formattedData.push({ image: rentalImgUrl, title: rentalName });
+                    const dataObj = {
+                        image: rentalImgUrl,
+                        title: rentalName,
+                        vehicleType: vehicleType,
+                        pricePerDay: pricePerDayFormatted,
+                        presentmentCurr: presentmentCurr
+                    };
+
+                    if (minPriceInputValue !== 0 && maxPriceInputValue !== 0) {
+                        if (pricePerDayFormatted >= minPriceInputValue && pricePerDayFormatted <= maxPriceInputValue) {
+                            formattedData.push(dataObj);
+                        }
+                    } else if (minPriceInputValue !== 0 && maxPriceInputValue === 0) {
+                        if (pricePerDayFormatted >= minPriceInputValue) {
+                            formattedData.push(dataObj);
+                        }
+                    } else if (maxPriceInputValue !== 0 && minPriceInputValue === 0) {
+                        if (pricePerDayFormatted <= maxPriceInputValue) {
+                            formattedData.push(dataObj);
+                        }
+                    } else if (maxPriceInputValue === 0 && minPriceInputValue === 0) {
+                        formattedData.push(dataObj);
+                    }
                 });
 
+                if (sortItemsAsc) formattedData.sort((a, b) => a.pricePerDay - b.pricePerDay);
+                else if (sortItemsDesc) formattedData.sort((a, b) => b.pricePerDay - a.pricePerDay);
+
+                setItemsCount(formattedData.length);
                 setList(formattedData);
                 setIsLoading(false);
             } catch (error) {
@@ -48,33 +86,67 @@ const Search: React.FC<Props> = ({ setList, setError, setIsLoading }) => {
                 setError(message);
                 setIsLoading(false);
             }
-        }, 500);
+        }, timeout);
 
         return () => clearTimeout(timer);
-    }, [inputValue]);
+    }, [searchInputValue, minPriceInputValue, maxPriceInputValue, sortItemsAsc, sortItemsDesc]);
 
-    const inputChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setInputValue(e.target.value);
+    const searchInputChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchInputValue(e.target.value);
+    };
+
+    const minPriceInputHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setMinPriceInputValue(+e.target.value);
+    };
+
+    const maxPriceInputHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setMaxPriceInputValue(+e.target.value);
+    };
+
+    const sortItemsAscHandler = (e: React.MouseEvent) => {
+        setSortItemsAsc(true);
+
+        if (sortItemsDesc) setSortItemsDesc(false);
+    };
+
+    const sortItemsDescHandler = (e: React.MouseEvent) => {
+        setSortItemsDesc(true);
+
+        if (sortItemsAsc) setSortItemsAsc(false);
     };
 
     return (
-        <div className={classes.container}>
-            <label className={classes.label}>Search: </label>
-            <input
-                className={classes.input}
-                value={inputValue}
-                type='text'
-                onChange={inputChangeHandler}
-                list='filter-types'
-                placeholder='trailer'
-            />
+        <>
+            <div className={classes.container}>
+                <label className={classes.label}>Search: </label>
+                <input
+                    className={classes.input_search}
+                    value={searchInputValue}
+                    type='text'
+                    onChange={searchInputChangeHandler}
+                    list='filter-types'
+                    placeholder='trailer'
+                />
 
-            <datalist id='filter-types'>
-                <option value='trailer' />
-                <option value='fifth-wheel' />
-                <option value='camper-van' />
-            </datalist>
-        </div>
+                <datalist id='filter-types'>
+                    <option value='trailer' />
+                    <option value='fifth-wheel' />
+                    <option value='camper-van' />
+                    <option value='a' label='Class A' />
+                    <option value='b' label='Class B' />
+                    <option value='c' label='Class C' />
+                </datalist>
+            </div>
+
+            <PriceFilter onMinPriceChange={minPriceInputHandler} onMaxPriceChange={maxPriceInputHandler} />
+
+            <ListItemsOrder
+                sortItemsDesc={sortItemsDesc}
+                sortItemsAsc={sortItemsAsc}
+                onSortItemsAscOrder={sortItemsAscHandler}
+                onSortItemsDescOrder={sortItemsDescHandler}
+            />
+        </>
     );
 };
 
